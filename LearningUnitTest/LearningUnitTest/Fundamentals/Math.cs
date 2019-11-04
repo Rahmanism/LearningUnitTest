7783a0820a7648c963e827be35e2d8f40a98d180
Rahmanism <rahmani@mailkh.com>
2019-11-04 17:18:34 +0330

    Math class is added.

---
+++ LearningUnitTest/LearningUnitTest/Fundamentals/Math.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LearningUnitTest.Fundamentals
+{
+    public class Math
+    {
+        public int Add( int a, int b )
+        {
+            return a + b;
+        }
+
+        public int Max( int a, int b )
+        {
+            return ( a > b ) ? a : b;
+        }
+
+        public IEnumerable<int> GetOddNumbers( int limit )
+        {
+            for ( var i = 0; i <= limit; i++ ) {
+                if ( i % 2 != 0 ) {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
