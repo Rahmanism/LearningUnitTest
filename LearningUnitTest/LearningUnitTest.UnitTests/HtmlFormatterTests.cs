18fb9b02bdf9fac06746fb166d04891862cda39b
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-05 19:45:22 +0330

    Not too specific or too general tests.

---
+++ LearningUnitTest/LearningUnitTest.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,28 @@
+﻿using LearningUnitTest.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTest.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+            
+            // Too specific - not good
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+            
+            // Too general - not good
+            // Assert.That(result, Does.StartWith("<strong>"));
+            
+            // Good test
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
