4f9cdb3205e4b968fcd014df7ddc9eb769392b74
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-06 22:28:29 +0330

    Test a command method that has no return value.

---
+++ LearningUnitTest/LearningUnitTest.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,26 @@
+﻿using LearningUnitTest.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTest.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+        
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            const string error = "a";
+            _logger.Log(error);
+            
+            Assert.That(_logger.LastError, Is.EqualTo(error));
+        }
+    }
+}
---
+++ LearningUnitTest/LearningUnitTest/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LearningUnitTest.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+
+        public void Log(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+
+            LastError = error;
+            // write log...
+            
+            ErrorLogged?.Invoke(this, Guid.NewGuid());
+        }
+    }
+}
