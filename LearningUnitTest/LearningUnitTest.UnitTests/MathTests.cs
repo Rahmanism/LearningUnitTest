b6968f3102402f2aef3ef8dc5de58e7c8cd4f25a
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-06 15:25:50 +0330

    One scenrio test for GetOddNumbers

--- LearningUnitTest/LearningUnitTest.UnitTests/MathTests.cs
+++ LearningUnitTest/LearningUnitTest.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
-﻿using LearningUnitTest.Fundamentals;
+﻿using System.Linq;
+using LearningUnitTest.Fundamentals;
 using NUnit.Framework;
 
 namespace LearningUnitTest.UnitTests
@@ -24,22 +25,42 @@ namespace LearningUnitTest.UnitTests
             // Arrange - is in SetUP
 
             // Act
-            var result = math.Add( 1, 2 );
+            var result = math.Add(1, 2);
 
             // Assert
-            Assert.That( result, Is.EqualTo( 3 ) );
+            Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
-        [TestCase( 2, 1, 2 )]
-        [TestCase( 1, 2, 2 )]
-        [TestCase( 1, 1, 1 )]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
         public void Max_WhenCalled_ReturnsTheGreaterArgument(
-            int a, int b, int expectedResult )
+            int a, int b, int expectedResult)
         {
-            var result = math.Max( a, b );
+            var result = math.Max(a, b);
 
-            Assert.That( result, Is.EqualTo( expectedResult ) );
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnsOddNumbersUpToLimit()
+        {
+            var result = math.GetOddNumbers(5);
+
+            // very general test - bad
+            Assert.That(result, Is.Not.Empty);
+
+            // a little more specific but we still can any wrong numbers.
+            Assert.That(result.Count(), Is.EqualTo(3));
+
+            // the better way
+            // order is not important so:
+            Assert.That(result, Is.EquivalentTo(new[] {1, 3, 5}));
+            
+            // if we want ordered or unique arrays
+            // Assert.That(result, Is.Ordered);
+            // Assert.That(result, Is.Unique);
         }
     }
 }
