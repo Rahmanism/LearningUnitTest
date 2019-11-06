53ece191726aa345d24ef1a7b29df5bfd203e68f
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-06 22:45:55 +0330

    Testing throw exception

--- LearningUnitTest/LearningUnitTest.UnitTests/ErrorLoggerTests.cs
+++ LearningUnitTest/LearningUnitTest.UnitTests/ErrorLoggerTests.cs
@@ -22,5 +22,14 @@ namespace LearningUnitTest.UnitTests
             
             Assert.That(_logger.LastError, Is.EqualTo(error));
         }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidInput_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+        }
     }
 }
