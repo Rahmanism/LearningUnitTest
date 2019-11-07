8228a3db072540af18647000f22abe9c61b9098e
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-07 16:33:17 +0330

    Exercise for throw exception

---
+++ LearningUnitTest/LearningUnitTest.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LearningUnitTest.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTest.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demerit;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demerit = new DemeritPointsCalculator();
+        }
+        
+        [Test]
+        public void CalculateDemeritPoints_InvalidArgument_ThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _demerit.CalculateDemeritPoints(-1));
+        }
+
+        [Test]
+        [TestCase(65, 0)]
+        [TestCase(85, 4)]
+        public void CalculateDemeritPoints_ValidArgument_ReturnDemeritPoints(
+            int speed, int expectedDemeritPoints)
+        {
+            var result = _demerit.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedDemeritPoints));
+        }
+    }
+}
---
+++ LearningUnitTest/LearningUnitTest/Fundamentals/DemeritPointsCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearningUnitTest.Fundamentals
+{
+    public class DemeritPointsCalculator
+    {
+        private const int SpeedLimit = 65;
+
+        public int CalculateDemeritPoints(int speed)
+        {
+            if (speed < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (speed <= SpeedLimit)
+                return 0;
+
+            const int kmPerDemeritPoint = 5;
+            int demeritPoints = (speed - SpeedLimit) / kmPerDemeritPoint;
+
+            return demeritPoints;
+        }
+    }
+}
