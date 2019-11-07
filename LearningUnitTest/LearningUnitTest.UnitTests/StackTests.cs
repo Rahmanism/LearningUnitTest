57ce122dba08991d1693f302a73876b7a16d3e64
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-07 17:59:47 +0330

    Exercise test for command methods.

---
+++ LearningUnitTest/LearningUnitTest.UnitTests/StackTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using LearningUnitTest.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTest.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Fundamentals.Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Fundamentals.Stack<string>();
+        }
+
+        [Test]
+        public void Push_ArgIsNull_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _stack.Push(null));
+        }
+
+        [Test]
+        public void Push_ArgIsValid_AddTheObjToStack()
+        {
+            _stack.Push("a");
+            
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_StackIsEmpty_ThrowInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _stack.Pop());
+        }
+
+        [Test]
+        public void Pop_StackWithObjects_ReturnTheObjOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            var result = _stack.Pop();
+            
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithObjects_RemoveTheTopObject()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            _stack.Pop();
+            
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+     
+        [Test]
+        public void Peek_StackIsEmpty_ThrowInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _stack.Pop());
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnTheObjOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            var result = _stack.Peek();
+            
+            Assert.That(result, Is.EqualTo("c"));
+        }
+        
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveTheTopObject()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            _stack.Peek();
+            
+            Assert.That(_stack.Count, Is.EqualTo(3));
+        }
+    }
+}
