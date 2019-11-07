e03b3376eafd9235d8cf2726d47f3112ff4a7c9b
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-07 18:45:09 +0330

    A mocking class (VideoService) is added.
    NewtonSoft.Json and EntityFramework are added to the main project.

---
+++ LearningUnitTest/LearningUnitTest/Mocking/VideoService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace LearningUnitTest.Mocking
+{
+    public class VideoService
+    {
+        public string ReadVideoTitle()
+        {
+            var str = File.ReadAllText("video.txt");
+            var video = JsonConvert.DeserializeObject<Video>(str);
+            if (video == null)
+                return "Error parsing the video.";
+            return video.Title;
+        }
+
+        public string GetUnprocessedVideosAcCsv()
+        {
+            var videoIds = new List<int>();
+
+            using (var context = new VideoContext()) {
+                var videos =
+                    (from video in context.Videos
+                        where !video.IsProcessed
+                        select video).ToList();
+                videoIds.AddRange(videos.Select(v => v.Id));
+
+                return String.Join(",", videoIds);
+            }
+        }
+    }
+
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsProcessed { get; set; }
+    }
+
+    public class VideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
