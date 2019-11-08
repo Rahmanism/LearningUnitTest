fac818ee066904ea9c90c9f0e2a10617b72192a2
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-08 23:35:39 +0330

    Moved initialization to SetUp in VideoService test.

--- LearningUnitTest/LearningUnitTest.UnitTests/Mocking/VideoServiceTests.cs
+++ LearningUnitTest/LearningUnitTest.UnitTests/Mocking/VideoServiceTests.cs
@@ -7,13 +7,22 @@ namespace LearningUnitTest.UnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _service;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _service = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var fileReader = new Mock<IFileReader>();
-            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
-            var service = new VideoService(fileReader.Object);
-            var result = service.ReadVideoTitle();
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+            
+            var result = _service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("Error").IgnoreCase);
         }
