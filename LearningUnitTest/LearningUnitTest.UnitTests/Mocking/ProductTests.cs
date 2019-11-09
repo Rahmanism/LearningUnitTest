20dcffafab63fe615ce95868da57bad332bddc1c
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-09 22:34:17 +0330

    Test without mock when it's not needed.

---
+++ LearningUnitTest/LearningUnitTest.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,18 @@
+﻿using LearningUnitTest.Mocking;
+using NUnit.Framework;
+
+namespace LearningUnitTest.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product {ListPrice = 100};
+            var result = product.GetPrice(new Customer {IsGold = true});
+            
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
