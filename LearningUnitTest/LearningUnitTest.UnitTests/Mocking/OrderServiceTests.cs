0cd5e1f0bd197a47ac110c9824dffaa08c52c259
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-11-09 22:10:01 +0330

    Testing interations between two object. OrderServie and Storage

--- LearningUnitTest/LearningUnitTest.UnitTests/Mocking/OrderServiceTests.cs
+++ LearningUnitTest/LearningUnitTest.UnitTests/Mocking/OrderServiceTests.cs
@@ -1,10 +1,30 @@
-﻿using NUnit.Framework;
+﻿using LearningUnitTest.Mocking;
+using Moq;
+using NUnit.Framework;
 
 namespace LearningUnitTest.UnitTests.Mocking
 {
     [TestFixture]
     public class OrderServiceTests
     {
-        
+        private OrderService _service;
+        private Mock<IStorage> _storage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _storage = new Mock<IStorage>();
+            _service = new OrderService(_storage.Object);
+        }
+
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var order = new Order();
+            _service.PlaceOrder(order);
+            
+            // Assert
+            _storage.Verify(s => s.Store(order));
+        }
     }
 }
